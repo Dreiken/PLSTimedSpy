3f3757a4fb7d69cec2ec451878feff2f7f169775
Dreiken <santidreik@gmail.com>
2016-11-11 13:15:40 -0300

    Add files via upload

--- PLSTimedSpy.cs
+++ PLSTimedSpy.cs
@@ -87,46 +87,95 @@ namespace PLSTimedSpy
         int CheckExist(UnturnedPlayer player, string DirectoryName, int Times)
         {
             DateTime[] creacion = new DateTime[(Times)];
-            for (int a = 0; a <= (Times - 1); a++)
-            {
-                if (File.Exists(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + a + ".jpg"))) ;
-                else
-                {
-                    return a;
-                }
-            }
-            for (int a = 0; a <= (Times - 1); a++)
-            {
-                creacion[a] =
-                File.GetLastWriteTime(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + a + ".jpg"));
-            }
-            if (creacion[0].CompareTo(creacion[Times - 1]) < 0)
-            {
-                if (this.Configuration.Instance.BackupSpy == true)
-                {
-                    File.Copy(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + 0 + ".jpg"),
-                   Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + "BackupSpy/" + creacion[0].Month + "." + creacion[0].Day + " " + player.DisplayName.ToString() + "_" + creacion[0].Hour + "h" + creacion[0].Minute + ".jpg"));
-                   File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + 0 + ".jpg"));
-                }
 
-                else File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + 0 + ".jpg"));
-                return 0;
-            }
-            for (int a = 0; a <= (Times - 1); a++)
+            if (!this.Configuration.Instance.OnlySteamId)
             {
-                if (creacion[(a + 1)].CompareTo(creacion[a]) < 0)
+                for (int a = 0; a <= (Times - 1); a++)
+                {
+                    if (File.Exists(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + a + ".jpg"))) ;
+                    else
+                    {
+                        return a;
+                    }
+                }
+                for (int a = 0; a <= (Times - 1); a++)
+                {
+                    creacion[a] =
+                    File.GetLastWriteTime(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + a + ".jpg"));
+                }
+                if (creacion[0].CompareTo(creacion[Times - 1]) < 0)
                 {
                     if (this.Configuration.Instance.BackupSpy == true)
                     {
-                        File.Copy(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + (a + 1) + ".jpg"),
-                   Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + "BackupSpy/" + creacion[a + 1].Month + "." + creacion[a + 1].Day + " " + player.DisplayName.ToString() + creacion[a + 1].Hour + "h" + creacion[a + 1].Minute + ".jpg"));
-                        File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + (a + 1) + ".jpg"));
+                        File.Copy(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + 0 + ".jpg"),
+                       Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + "BackupSpy/" + creacion[0].Month + "." + creacion[0].Day + " " + player.DisplayName.ToString() + "_" + creacion[0].Hour + "h" + creacion[0].Minute + ".jpg"));
+                        File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + 0 + ".jpg"));
                     }
 
-                    else File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + (a + 1) + ".jpg"));
-                    return (a + 1);
+                    else File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + 0 + ".jpg"));
+                    return 0;
+                }
+                for (int a = 0; a <= (Times - 1); a++)
+                {
+                    if (creacion[(a + 1)].CompareTo(creacion[a]) < 0)
+                    {
+                        if (this.Configuration.Instance.BackupSpy == true)
+                        {
+                            File.Copy(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + (a + 1) + ".jpg"),
+                       Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + "BackupSpy/" + creacion[a + 1].Month + "." + creacion[a + 1].Day + " " + player.DisplayName.ToString() + creacion[a + 1].Hour + "h" + creacion[a + 1].Minute + ".jpg"));
+                            File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + (a + 1) + ".jpg"));
+                        }
+
+                        else File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.DisplayName.ToString() + "_" + player.CSteamID.ToString() + "_" + (a + 1) + ".jpg"));
+                        return (a + 1);
+                    }
                 }
             }
+
+            else
+            {
+                for (int a = 0; a <= (Times - 1); a++)
+                {
+                    if (File.Exists(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.CSteamID.ToString() + "_" + a + ".jpg"))) ;
+                    else
+                    {
+                        return a;
+                    }
+                }
+                for (int a = 0; a <= (Times - 1); a++)
+                {
+                    creacion[a] =
+                    File.GetLastWriteTime(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.CSteamID.ToString() + "_" + a + ".jpg"));
+                }
+                if (creacion[0].CompareTo(creacion[Times - 1]) < 0)
+                {
+                    if (this.Configuration.Instance.BackupSpy == true)
+                    {
+                        File.Copy(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.CSteamID.ToString() + "_" + 0 + ".jpg"),
+                       Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + "BackupSpy/" + creacion[0].Month + "." + creacion[0].Day + " " + player.CSteamID.ToString() + "_" + creacion[0].Hour + "h" + creacion[0].Minute + ".jpg"));
+                        File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.CSteamID.ToString() + "_" + 0 + ".jpg"));
+                    }
+
+                    else File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.CSteamID.ToString() + "_" + 0 + ".jpg"));
+                    return 0;
+                }
+                for (int a = 0; a <= (Times - 1); a++)
+                {
+                    if (creacion[(a + 1)].CompareTo(creacion[a]) < 0)
+                    {
+                        if (this.Configuration.Instance.BackupSpy == true)
+                        {
+                            File.Copy(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.CSteamID.ToString() + "_" + (a + 1) + ".jpg"),
+                       Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + "BackupSpy/" + creacion[a + 1].Month + "." + creacion[a + 1].Day + " " + player.CSteamID.ToString() + creacion[a + 1].Hour + "h" + creacion[a + 1].Minute + ".jpg"));
+                            File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.CSteamID.ToString() + "_" + (a + 1) + ".jpg"));
+                        }
+
+                        else File.Delete(Path.GetFullPath(serverFolder + "/PleaseGamingFunctionalities/" + DirectoryName + "/" + player.CSteamID.ToString() + "_" + (a + 1) + ".jpg"));
+                        return (a + 1);
+                    }
+                }
+            }
+
             return 0;
         }
 
